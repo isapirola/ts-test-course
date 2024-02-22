064ae72d7bc106e7842a669bf9074c0581c527d2
IsaPirola <belapirola@gmail.com>
2024-02-22 16:54:24 -0300

    implement admin requirement

--- src/app/pass_checker/PasswordChecker.ts
+++ src/app/pass_checker/PasswordChecker.ts
@@ -2,6 +2,7 @@ export enum PasswordErrors {
     SHORT = "Password is too short!",
     NO_UPPER_CASE = "Upper case letter required",
     NO_LOWER_CASE = "Lower case letter required",
+    NO_NUMBER = "At least one number required",
 }
 
 export interface CheckResult {
@@ -21,6 +22,23 @@ export class PasswordChecker {
             valid: reasons.length > 0 ? false : true,
             reasons: reasons,
         };
+    }
+
+    public checkAdminPassword(password: string): CheckResult {
+        const basicCheck = this.checkPassword(password);
+        this.checkForNumber(password, basicCheck.reasons);
+
+        return {
+            valid: basicCheck.reasons.length > 0 ? false : true,
+            reasons: basicCheck.reasons,
+        };
+    }
+
+    private checkForNumber(password: string, reasons: PasswordErrors[]) {
+        const hasNumber = /\d/;
+        if (!hasNumber.test(password)) {
+            reasons.push(PasswordErrors.NO_NUMBER);
+        }
     }
 
     private checkForLength(password: string, reasons: PasswordErrors[]) {
