996e21c57147540f9171402205c19843282b5367
IsaPirola <belapirola@gmail.com>
2024-02-22 16:38:38 -0300

    implement password check test

--- src/test/pass_checker/PasswordChecker.test.ts
+++ src/test/pass_checker/PasswordChecker.test.ts
@@ -1,4 +1,4 @@
-import { PasswordChecker } from "../../app/pass_checker/PasswordChecker";
+import { PasswordChecker, PasswordErrors } from "../../app/pass_checker/PasswordChecker";
 
 describe("PasswordChecker test suite", () => {
     let sut: PasswordChecker;
@@ -7,7 +7,42 @@ describe("PasswordChecker test suite", () => {
         sut = new PasswordChecker();
     });
 
-    it("Should do nothing for the moment", () => {
-        sut.checkPassword();
+    it("Password with less than 8 chars is invalid", () => {
+        const actual = sut.checkPassword("1234567");
+        expect(actual.valid).toBe(false);
+        expect(actual.reasons).toContain(PasswordErrors.SHORT);
+    });
+
+    it("Password with more than 8 chars is ok", () => {
+        const actual = sut.checkPassword("12345678");
+        expect(actual.reasons).not.toContain(PasswordErrors.SHORT);
+    });
+
+    it("Password with no upper case is invalid", () => {
+        const actual = sut.checkPassword("abcd");
+        expect(actual.valid).toBe(false);
+        expect(actual.reasons).toContain(PasswordErrors.NO_UPPER_CASE);
+    });
+
+    it("Password with upper case is valid", () => {
+        const actual = sut.checkPassword("aBcd");
+        expect(actual.reasons).not.toContain(PasswordErrors.NO_UPPER_CASE);
+    });
+
+    it("Password with no lower case is invalid", () => {
+        const actual = sut.checkPassword("ABCD");
+        expect(actual.valid).toBe(false);
+        expect(actual.reasons).toContain(PasswordErrors.NO_LOWER_CASE);
+    });
+
+    it("Password with lower case is valid", () => {
+        const actual = sut.checkPassword("aBcd");
+        expect(actual.reasons).not.toContain(PasswordErrors.NO_LOWER_CASE);
+    });
+
+    it("Complex password is valid", () => {
+        const actual = sut.checkPassword("1234abcD");
+        expect(actual.reasons).toHaveLength(0);
+        expect(actual.valid).toBe(true);
     });
 });
