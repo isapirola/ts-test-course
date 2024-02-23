73863f9133befb0efd6af81f77b992be665a9323
IsaPirola <belapirola@gmail.com>
2024-02-23 10:45:59 -0300

    introdution to fakes

--- src/app/doubles/OtherUtils.ts
+++ src/app/doubles/OtherUtils.ts
@@ -9,3 +9,14 @@ export type stringInfo = {
 export function calculateComplexity(stringInfo: stringInfo) {
     return Object.keys(stringInfo.extraInfo).length * stringInfo.length;
 }
+
+type LoggerServiceCallback = (arg: string) => void;
+
+export function toUpperCaseWithCb(arg: string, callBack: Function) {
+    if (!arg) {
+        callBack("Invalid argument!");
+        return;
+    }
+    callBack(`Called function with ${arg}`);
+    return arg.toUpperCase();
+}
