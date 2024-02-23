b51b0870e2c2f3ab1d973b896fd0a00a4b54a75b
IsaPirola <belapirola@gmail.com>
2024-02-23 16:21:41 -0300

    finish RegisterHandler test

--- src/test/server_app/handlers/RegisterHandler.test.ts
+++ src/test/server_app/handlers/RegisterHandler.test.ts
@@ -1,6 +1,8 @@
 import { RegisterHandler } from "../../../app/server_app/handlers/RegisterHandler";
 import { IncomingMessage, ServerResponse } from "http";
 import { Authorizer } from "../../../app/server_app/auth/Authorizer";
+import { HTTP_CODES, HTTP_METHODS } from "../../../app/server_app/model/ServerModel";
+import { Account } from "../../../app/server_app/model/AuthModel";
 
 const getRequestBodyMock = jest.fn();
 
@@ -25,6 +27,14 @@ describe("RegisterHandler test suite", () => {
         registerUser: jest.fn(),
     };
 
+    const someAccount: Account = {
+        id: "",
+        password: "somePassword",
+        userName: "someUserName",
+    };
+
+    const someId = "1234";
+
     beforeEach(() => {
         sut = new RegisterHandler(
             request as IncomingMessage,
@@ -35,5 +45,23 @@ describe("RegisterHandler test suite", () => {
 
     afterEach(() => {
         jest.clearAllMocks();
+    });
+
+    it("should register valid accounts in requests", async () => {
+        request.method = HTTP_METHODS.POST;
+        getRequestBodyMock.mockResolvedValueOnce(someAccount);
+        authorizerMock.registerUser.mockResolvedValueOnce(someId);
+
+        await sut.handleRequest();
+
+        expect(responseMock.statusCode).toBe(HTTP_CODES.CREATED);
+        expect(responseMock.writeHead).toHaveBeenCalledWith(HTTP_CODES.CREATED, {
+            "Content-Type": "application/json",
+        });
+        expect(responseMock.write).toHaveBeenCalledWith(
+            JSON.stringify({
+                userId: someId,
+            })
+        );
     });
 });
